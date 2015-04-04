bc3dc0bba96ffe925d67f3e4d74e69350a8f70ed
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-04 22:49:03 +0200

    Added error handling for jwt errors

--- rest.js
+++ rest.js
@@ -2,8 +2,9 @@ module.exports = function (app) {
 
   var jwt = require('express-jwt')({secret: app.secret});
 
-  var Users = require('./routes/r_user')(app)
-  var Orders = require('./routes/r_orders')(app)
+  var Users = require('./routes/r_user')(app);
+  var Orders = require('./routes/r_orders')(app);
+  var util = require('./util');
 
   // User login
   app.post('/api/users/login', Users.login);
@@ -13,8 +14,8 @@ module.exports = function (app) {
   // Only if User logged in
   app.get('/api/orders/:id', jwt, Orders.getById);
   // Only if User logged in
-  app.get('/api/users/self/orders', jwt, Orders.getOrders);
+  app.get('/api/users/self/orders', jwt, Orders.getOrders, util.sendAuthError);
   // Only if User logged in
-  app.post('/api/orders', jwt, Orders.create);
+  app.post('/api/users/self/orders', jwt, Orders.create,  util.sendAuthError);
 
 }
