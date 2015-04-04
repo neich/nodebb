47b00eac1c54951fc264546a8de7e67a9727eaf8
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-04 22:52:12 +0200

    Pass dao object to dao modules to allow interoperability

--- dao/index.js
+++ dao/index.js
@@ -5,8 +5,8 @@
 module.exports = function (app) {
   var dao = {};
 
-  dao.User = require('./d_user')(app);
-  dao.Order = require('./d_order')(app);
+  dao.User = require('./d_user')(app, dao);
+  dao.Order = require('./d_order')(app, dao);
 
   return dao;
 }
