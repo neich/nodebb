072914200c6697f24ef55cc35c9002339fc1b514
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-04 22:51:24 +0200

    Fixed Order operations

--- dao/d_order.js
+++ dao/d_order.js
@@ -2,36 +2,31 @@
  * New node file
  */
 
-module.exports = function (app) {
+module.exports = function (app, dao) {
   var util = require('../util');
   var db = app.db;
   var P = app.Promise;
-  var dao = {};
+  var Order = {};
 
-  dao.getById = function (id, t) {
+  Order.getById = function (id, t) {
     return db.Order.find(util.addTrans(t, {where: {id: id}}));
   }
 
-  dao.getUserOrders = function (username, options, t) {
-    return dao.User.getByUsername(username, util.addTrans(t, {}))
-      .then(function(user) {
-        if (!user) util.throwError(200, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no User with username: ' + username);
-        return user.getOrders(options);
-      })
-  }
-
-  dao.create = function (order_data, username, t) {
+  Order.getUserOrders = function (username, options, t) {
+    var opt = options || {};
     return dao.User.getByUsername(username, t)
       .then(function(user) {
-        return db.Order.create(order_data, util.addTrans(t, {})).then(function(order) {
-          return {user: user, order: order}
-        });
+        if (!user) util.throwError(200, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no User with username: ' + username);
+        return user.getOrders(util.addTrans(t, opt));
       })
-      .catch(util.throwError(200, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no User with username: ' + username))
-      .then(function (result) {
-        return result.order.setUser(result.user, util.addTrans(t, {}))
-      });
   }
 
-  return dao;
+  Order.create = function (order_data, user, t) {
+    return db.Order.create(order_data, util.addTrans(t, {}))
+      .then(function(order) {
+          return order.setUser(user, util.addTrans(t, {}))
+        });
+  }
+
+  return Order;
 }
