26f715f915c408911957ad549328f07e2fb81e6a
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-04 22:50:01 +0200

    Fixed error handling bugs. Added error types

--- util/index.js
+++ util/index.js
@@ -14,8 +14,7 @@ exports.resendError = function (res, err) {
   var code = err.code || 500;
   var stack = err.stack;
   var json = {};
-  json.type = err.type || "ERROR_UNKNOWN";
-  json.name = err.name || "UNKNOWN";
+  json.type = err.type || error.ERR_UNKNOWN;
   json.message = err.message || "Unknown error";
   res.status(code).json({
     error: json
@@ -24,10 +23,18 @@ exports.resendError = function (res, err) {
 
 exports.sendError = function (res, code, type, reason) {
   var json = {};
-  json.type = type || "ERROR_UNKNOWN";
+  json.type = type || error.ERR_UNKNOWN;
   json.message = reason || "Unknown error";
   res.status(code || 500).json({ error: json });
 }
+
+exports.sendAuthError = function (err, req, res, next) {
+  var json = {};
+  json.type = error.ERR_AUTHENTICATION;
+  json.message = err.message || "Authentication error";
+  res.status(400).json({ error: json });
+}
+
 
 exports.addTrans = function (t, obj) {
   if (!t) return obj;
@@ -38,6 +45,7 @@ exports.addTrans = function (t, obj) {
 }
 
 exports.checkParams = function (obj, params) {
+  if (!obj) exports.throwError(400, error.ERR_BAD_REQUEST, "No body found in request");
   for (var i = 0; i < params.length; i++) {
     if (!obj.hasOwnProperty(params[i])) {
       exports.throwError(400, error.ERR_MISSING_PARAMETER, "Missing parameter (" + params[i] + ") in request");
@@ -50,7 +58,10 @@ var error = {
   ERR_AUTHENTICATION: 'ERR_AUTHENTICATION',
   ERR_ENTITY_NOT_FOUND: 'ERR_ENTITY_NOT_FOUND',
   ERR_ENTITY_EXISTS: 'ERR_ENTITY_EXISTS',
-  ERR_TRANSACTION: 'ERR_DB_TRANSACTION'
+  ERR_TRANSACTION: 'ERR_DB_TRANSACTION',
+  ERR_UNKNOWN: 'ERR_UNKNOWN',
+  ERR_BAD_REQUEST: 'ERR_BAD_REQUEST'
+
 }
 
 exports.Error = error;
