229a1c57d834d9d985a4d0715e53afb2b09613bf
Ignacio Martín <ignacio.martin@udg.edu>
2015-04-10 13:29:55 +0200

    Added convenience functions to Backbone to store objects in localStorage (uses JSON) Added jwt token to sync Backbone function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,9 +16,9 @@ require([
 ], function (P, App) {
 
   // Hack !!
-  P.defer =  function () {
+  P.defer = function () {
     var resolve, reject;
-    var promise = new P(function() {
+    var promise = new P(function () {
       resolve = arguments[0];
       reject = arguments[1];
     });
@@ -28,6 +28,38 @@ require([
       promise: promise
     };
   }
+
+  // Add convenience JSON parse/stringigy functions to Backbone
+  Backbone.localStorage = {};
+  Backbone.localStorage.setItem = function (key, obj) {
+    window.localStorage.setItem(key, JSON.stringify(obj));
+  }
+  Backbone.localStorage.getItem = function (key) {
+    var str = window.localStorage.getItem(key);
+    if (str) {
+      return JSON.parse(str);
+    }
+  }
+
+  // WARNING: this should not go in production !!!!
+  window.localStorage.removeItem('user');
+
+  // Add JWT token to each Backbone sync call
+  var backboneSync = Backbone.sync;
+  Backbone.sync = function (method, model, options) {
+    var user = Backbone.localStorage.getItem('user');
+    var token = user && user.jwt;
+
+    if (token) {
+      options.headers = {
+        'Authorization': 'Bearer ' + token
+      }
+    }
+
+    // call the original function
+    backboneSync(method, model, options);
+  };
+
 
   // The "app" dependency is passed in as "App"
   App.initialize();
