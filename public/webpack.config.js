a2002a5a8d756159a3a3330a4a11a82e23f53278
Ignacio Martín <ignacio.martin@udg.edu>
2017-03-24 12:12:42 +0100

    Added support for bootstrap

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,31 +1,47 @@
-var debug = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: __dirname +"/js/main.js",
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: [
+    'font-awesome-loader',
+    __dirname +'/js/main.js'
+  ],
   output: {
-    path: __dirname + "/js",
-    filename: "main.min.js"
+    path: __dirname,
+    filename: 'js/main.min.js'
   },
   plugins: debug ? [new webpack.ProvidePlugin({
-      $ : "jquery",
-      Backbone : "backbone",
-      _ : "underscore"
+      $ : 'jquery',
+      Backbone : 'backbone',
+      _ : 'underscore'
     })] : [
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
       new webpack.ProvidePlugin({
-        $ : "jquery",
-        Backbone : "backbone",
-        _ : "underscore"
+        $ : 'jquery',
+        Backbone : 'backbone',
+        _ : 'underscore'
       })
     ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader' }
+      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&name=fonts/[name].[ext]',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader?name=fonts/[name].[ext]',
+      },
+
+      { test: /bootstrap-sass\/assets\/javascripts\//, use: 'imports-loader?jQuery=jquery' },
+
     ]
   }
 };
