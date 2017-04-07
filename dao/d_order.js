97413cf0308d9c19616c48f08a7fc3d5756666d7
Ignacio Martín <ignacio.martin@udg.edu>
2017-04-07 18:06:05 +0200

    Make getById() reject the promise if User is not found Use user id to get the orders

--- dao/d_order.js
+++ dao/d_order.js
@@ -9,14 +9,18 @@ module.exports = function (app, dao) {
   var Order = {};
 
   Order.getById = function (id, t) {
-    return db.Order.find(util.addTrans(t, {where: {id: id}}));
+    return db.Order.find(util.addTrans(t, {where: {id: id}}))
+      .then(function(order) {
+        if (!order) util.throwError(400, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no Order with id: ' + id)
+        else return order
+      })
   }
 
-  Order.getUserOrders = function (username, options, t) {
+  Order.getUserOrders = function (userId, options, t) {
     var opt = options || {};
-    return dao.User.getByUsername(username, t)
+    return dao.User.getById(userId, t)
       .then(function(user) {
-        if (!user) util.throwError(200, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no User with username: ' + username);
+        if (!user) util.throwError(400, util.Error.ERR_ENTITY_NOT_FOUND, 'There is no User with id: ' + userId);
         return user.getOrders(util.addTrans(t, opt));
       })
   }
