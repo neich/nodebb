ecf86ea5a80e38d6cce73066b803795b7d210cc2
Ignacio Martín <ignacio.martin@udg.edu>
2017-05-18 19:48:33 +0200

    Do not logout user on init

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,4 @@ require('bootstrap-loader');
 var localStorage = require('./localStorage')
 var App = require('./app')
 
-// WARNING: this deletes any previously user
-// THIS SHOULD NOT GO INTO PRODUCTION
-localStorage.removeItem('user')
-
 App.init()
