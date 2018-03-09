8300cc50070bd52cacc6c2f60c97ad539fb1e237
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-09 12:53:27 +0100

    Add firebase upload support

--- json-server/server.js
+++ json-server/server.js
@@ -1,21 +1,37 @@
-const path = require('path');
+const path = require('path')
 
 // json-server
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 
+// Firebase
+var firebase = require("firebase/app")
+require("firebase/storage")
+
+var config = {
+    apiKey: "AIzaSyBMYhMIMNHY3wgT9K2TZTYScTaTqLIm83o",
+    authDomain: "nodebb-edb15.firebaseapp.com",
+    databaseURL: "https://nodebb-edb15.firebaseio.com",
+    projectId: "nodebb-edb15",
+    storageBucket: "",
+    messagingSenderId: "626231727792"
+}
+firebase.initializeApp(config)
+
 // Routes
 const router = jsonServer.router('db.json')
 const addAuth = require('./auth')
+const addFileUpload = require('./upload')
 
 // Middleware
 const middlewares = jsonServer.defaults()
 const morgan = require('morgan')
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const serveStatic = require('serve-static');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const serveStatic = require('serve-static')
 const session = require('express-session')
-const cors = require('cors');
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
 
 const lodashId = require('lodash-id')
 
@@ -23,9 +39,9 @@ server.use(middlewares)
 server.use(morgan('combined'))
 server.use(bodyParser.json())
 server.use(function (error, req, res, next) {
-    error.location = "ERROR_JSON_PARSE";
-    error.code = 400;
-    next(error);
+    error.location = "ERROR_JSON_PARSE"
+    error.code = 400
+    next(error)
 })
 server.use(methodOverride())
 server.use(cors())
@@ -34,12 +50,14 @@ server.use(session({
     resave: true,
     saveUninitialized: true
 }))
+server.use(fileUpload())
 server.use(serveStatic(path.join(__dirname, '../public'), {'index': ['index.html', 'index.htm']}))
 
 // Add id-based entities to the database
 router.db._.mixin(lodashId)
 
 addAuth(server, router)
+addFileUpload(server, router, firebase.storage().ref())
 
 server.use(router)
 
