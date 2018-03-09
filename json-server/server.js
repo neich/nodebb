8b0f6f52da710799f90de175b1834dac151ee76d
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-09 18:36:59 +0100

    Remove firebase and add custom uploads

--- json-server/server.js
+++ json-server/server.js
@@ -4,24 +4,10 @@ const path = require('path')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 
-// Firebase
-var firebase = require("firebase/app")
-require("firebase/storage")
-
-var config = {
-    apiKey: "AIzaSyBMYhMIMNHY3wgT9K2TZTYScTaTqLIm83o",
-    authDomain: "nodebb-edb15.firebaseapp.com",
-    databaseURL: "https://nodebb-edb15.firebaseio.com",
-    projectId: "nodebb-edb15",
-    storageBucket: "",
-    messagingSenderId: "626231727792"
-}
-firebase.initializeApp(config)
-
 // Routes
 const router = jsonServer.router('db.json')
 const addAuth = require('./auth')
-const addFileUpload = require('./upload')
+const addFileUpload = require('./images')
 
 // Middleware
 const middlewares = jsonServer.defaults()
@@ -45,19 +31,19 @@ server.use(function (error, req, res, next) {
 })
 server.use(methodOverride())
 server.use(cors())
+server.use(fileUpload())
 server.use(session({
     secret: '63?gdº93!6dg36dºb36%Vv57V%c$%/(!V497',
     resave: true,
     saveUninitialized: true
 }))
-server.use(fileUpload())
 server.use(serveStatic(path.join(__dirname, '../public'), {'index': ['index.html', 'index.htm']}))
 
 // Add id-based entities to the database
 router.db._.mixin(lodashId)
 
 addAuth(server, router)
-addFileUpload(server, router, firebase.storage().ref())
+addFileUpload(server, router)
 
 server.use(router)
 
