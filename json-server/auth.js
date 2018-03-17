f87add4f7bf52cbbafb48d2cde9840e8620ba18c
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-17 13:06:16 +0100

    Use util.isAuthenticated. Convert userId to string for query parameter

--- json-server/auth.js
+++ json-server/auth.js
@@ -37,42 +37,30 @@ module.exports = function (server, router) {
     function addAuthorization(server, entity) {
 
         function authMiddleware_GET_PUT_DELETE(req, res, next) {
-            if (!req.session.userId)
-                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'No authenticated user')
-            else {
-                var obj = router.db.get(entity).find(['id', parseInt(req.params.id)]).value()
-                if (!obj)
-                    util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'Object does not exists')
-                else if (obj.userId !== req.session.userId)
-                    util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'You don\'t have permissions to acces this object')
-                else
-                    next()
-            }
+            var obj = router.db.get(entity).find(['id', parseInt(req.params.id)]).value()
+            if (!obj)
+                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'Object does not exists')
+            else if (obj.userId !== req.session.userId)
+                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'You don\'t have permissions to acces this object')
+            else
+                next()
         }
 
         function authMiddleware_POST(req, res, next) {
-            if (!req.session.userId)
-                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'No authenticated user')
-            else {
-                req.body.userId = req.session.userId
-                next()
-            }
+            req.body.userId = req.session.userId
+            next()
         }
 
         function authMiddleware_GET_Collection(req, res, next) {
-            if (!req.session.userId)
-                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'No authenticated user')
-            else {
-                req.query.userId = req.session.userId
-                next()
-            }
+            req.query.userId = req.session.userId.toString()
+            next()
         }
 
-        server.get('/' + entity + '/:id', authMiddleware_GET_PUT_DELETE);
-        server.put('/' + entity + '/:id', authMiddleware_GET_PUT_DELETE);
-        server.delete('/' + entity + '/:id', authMiddleware_GET_PUT_DELETE);
-        server.post('/' + entity, authMiddleware_POST)
-        server.get('/' + entity, authMiddleware_GET_Collection)
+        server.get('/' + entity + '/:id', util.isAuthenticated, authMiddleware_GET_PUT_DELETE);
+        server.put('/' + entity + '/:id', util.isAuthenticated, authMiddleware_GET_PUT_DELETE);
+        server.delete('/' + entity + '/:id', util.isAuthenticated, authMiddleware_GET_PUT_DELETE);
+        server.post('/' + entity, util.isAuthenticated, authMiddleware_POST)
+        server.get('/' + entity, util.isAuthenticated, authMiddleware_GET_Collection)
     }
 
     // Call this function for each entity that has ownership wrt users
@@ -100,34 +88,32 @@ module.exports = function (server, router) {
     })
 
     server.post('/users/logout', util.isAuthenticated, function (req, res) {
-        if (!req.session.userId)
-            util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'No authenticated user')
-        else {
-            delete req.session['userId']
-            delete req.session['username']
-            util.jsonResponse(res, 'User logged out successfully')
-        }
+        delete req.session['userId']
+        delete req.session['username']
+        util.jsonResponse(res, 'User logged out successfully')
     })
 
     server.post('/users', util.isNotAuthenticated, function (req, res) {
-        if (req.session.userId)
-            util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'You are already logged in')
+        var v = validate(req.body, userSchemaRegister)
+        if (!v.valid)
+            util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, util.jsonSchemaError(v))
         else {
-            var v = validate(req.body, userSchemaRegister)
-            if (!v.valid)
-                util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, util.jsonSchemaError(v))
-            else {
-                router.db
-                    .get('users')
-                    .insert({
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: req.body.password
-                    })
-                    .write()
-                util.jsonResponse(res, 'User created successfully')
-            }
+            router.db
+                .get('users')
+                .insert({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                .write()
+            util.jsonResponse(res, 'User created successfully')
         }
     })
+
+    server.get('/users/self', util.isAuthenticated, function (req, res) {
+        var user = router.db.get('users').find(['id', req.session.userId]).value()
+        util.jsonResponse(res, user)
+    })
+
 
 }
