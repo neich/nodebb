58ab9daa0530e3ea5ceb39615cc3b04eb7c7fba7
Ignacio Martín <ignacio.martin@udg.edu>
2018-04-24 14:51:25 +0200

    Post images fixed

--- json-server/images.js
+++ json-server/images.js
@@ -6,29 +6,26 @@ const path = require('path')
 
 module.exports = function (server, router) {
 
-    server.post('/images', util.isAuthenticated, function (req, res) {
-        Object.getOwnPropertyNames(req.files).forEach(function (fileName) {
-            var file = req.files[fileName]
-            var fname = uuidv4() + '.' + mime.extension(file.mimetype)
-            file.mv('./data/' + fname , function (err) {
-                if (err)
-                    util.sendError(res, 500, util.Error.ERR_UNKNOWN, err)
-                else {
-                    router.db.get('images')
-                        .insert({filename: fname, userId: req.session.userId})
-                        .write()
-                    util.jsonResponse(res, 'File uploaded!');
-                }
-            })
-        })
+  server.post('/images', util.isAuthenticated, function (req, res) {
+    var images = []
+    Object.getOwnPropertyNames(req.files).forEach(function (fileName) {
+      var file = req.files[fileName]
+      var fname = uuidv4() + '.' + mime.extension(file.mimetype)
+      fs.writeFileSync('./data/' + fname, file.data)
+      router.db.get('images')
+        .insert({filename: fname, userId: req.session.userId})
+        .write()
+      images.push(fname)
     })
+    util.jsonResponse(res, images);
+  })
 
-    server.get('/images/:filename', util.isAuthenticated, function(req, res) {
-        var file = router.db.get('images').find(['filename', req.params.filename]).value()
-        if (file)
-            res.sendFile(path.resolve('./data/' + file.filename))
-        else
-            util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'Image dos not exists')
-    })
+  server.get('/images/:filename', util.isAuthenticated, function (req, res) {
+    var file = router.db.get('images').find(['filename', req.params.filename]).value()
+    if (file)
+      res.sendFile(path.resolve('./data/' + file.filename))
+    else
+      util.sendError(res, 400, util.Error.ERR_BAD_REQUEST, 'Image dos not exists')
+  })
 }
 
