39e5e37e60fb9625c71dd99545498b9a0a26a0d5
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 10:52:49 +0100

    Use the correct template

--- src/js/views/order/v_orderItem.js
+++ src/js/views/order/v_orderItem.js
@@ -1,13 +1,13 @@
 import Backbone from 'backbone';
 import _ from 'underscore';
 
-const tl_orderItem = require("raw-loader!../../../templates/order/tl_orderItem.html");
+const t_orderItem = require("raw-loader!../../../templates/order/t_orderItem.html");
 
 const OrderItemView = Backbone.View.extend({
 
   initialize: function(params) {
     this.eventBus = params.eventBus;
-    this.template = _.template(tl_orderItem);
+    this.template = _.template(t_orderItem);
   },
 
   render: function () {
