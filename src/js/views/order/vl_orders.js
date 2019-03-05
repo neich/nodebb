4cc14f0970370a0fb0fd582f150b461c3a6a4ba7
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 13:51:21 +0100

    Add logic to manage order creation

--- src/js/views/order/vl_orders.js
+++ src/js/views/order/vl_orders.js
@@ -2,6 +2,7 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import OrderItemView from '~/views/order/v_orderItem';
 import OrderDetailView from '~/views/order/v_orderDetail';
+import OrderCreateView from '~/views/order/v_orderCreate';
 
 const tl_order = require("raw-loader!../../../templates/order/tl_order.html");
 
@@ -15,9 +16,17 @@ const OrderListView = Backbone.View.extend({
     this.localEventBus.on('view:order:showDetail', this.showDetail.bind(this));
     this.localEventBus.on('view:order:hideDetail', this.hideDetail.bind(this));
 
+    this.listenTo(this.localEventBus, 'view:orderCreate:created', this.orderCreated.bind(this));
+
+    this.listenTo(this.collection, 'add', this.appendOrder.bind(this));
+
   },
 
   className: 'container',
+
+  events: {
+    'click [data-action="add"]' : 'showCreate'
+  },
 
   render: function () {
     this.$el.html(this.template({orders: this.collection}));
@@ -44,6 +53,25 @@ const OrderListView = Backbone.View.extend({
   hideDetail: function() {
     this.$el.find('.order-detail').hide();
     this.$el.find('.order-list').addClass('col-md-12').removeClass('col-md-9');
+  },
+
+  showCreate: function() {
+    this.$el.find('.order-list').addClass('col-md-9').removeClass('col-md-12');
+    const $orderDetail = this.$el.find('.order-detail');
+    const localEventBus = this.localEventBus;
+    new OrderCreateView({el: $orderDetail, eventBus: localEventBus}).render();
+    $orderDetail.show();
+  },
+
+  orderCreated: function(order) {
+    this.collection.add(order);
+    this.hideDetail();
+  },
+
+  appendOrder: function(order) {
+    const $orderList = this.$el.find('.list-group');
+    const localEventBus = this.localEventBus;
+    $orderList.append(new OrderItemView({model: order, eventBus: localEventBus}).render().el);
   }
 
 });
