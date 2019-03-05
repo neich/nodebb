d778bad87174ac8792502be99d517713c9bfadd2
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 11:12:34 +0100

    Add click event to manage order item interaction

--- src/js/views/order/v_orderItem.js
+++ src/js/views/order/v_orderItem.js
@@ -10,9 +10,17 @@ const OrderItemView = Backbone.View.extend({
     this.template = _.template(t_orderItem);
   },
 
+  events: {
+    'click li': 'detail'
+  },
+
   render: function () {
     this.$el.html(this.template({order: this.model}));
     return this;
+  },
+
+  detail: function() {
+    this.eventBus.trigger('view:order:detail', this.model.get('id'));
   }
 
 });
