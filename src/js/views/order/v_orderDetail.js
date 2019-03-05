a8ee684c28e0a689e0c1de2d39e97a395ff2f61a
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 12:10:37 +0100

    Capture the click event on the icon and trigger an event to the event bus

--- src/js/views/order/v_orderDetail.js
+++ src/js/views/order/v_orderDetail.js
@@ -5,14 +5,22 @@ const t_orderDetail = require("raw-loader!../../../templates/order/t_orderDetail
 
 const OrderDetailView = Backbone.View.extend({
 
-    initialize: function(params) {
+    initialize: function (params) {
         this.eventBus = params.eventBus;
         this.template = _.template(t_orderDetail);
+    },
+
+    events: {
+        'click .close': 'hide'
     },
 
     render: function () {
         this.$el.html(this.template({order: this.model}));
         return this;
+    },
+
+    hide: function () {
+        this.eventBus.trigger('view:order:hideDetail');
     }
 
 });
