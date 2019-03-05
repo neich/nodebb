9d5c1eefb33e4ee59eae0c0a11971ea93a2998b2
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 11:34:04 +0100

    Create local event bus to manage events from child views (order items). Show order detail when capturing event from items

--- src/js/views/order/vl_orders.js
+++ src/js/views/order/vl_orders.js
@@ -1,6 +1,7 @@
 import Backbone from 'backbone';
 import _ from 'underscore';
 import OrderItemView from '~/views/order/v_orderItem';
+import OrderDetailView from '~/views/order/v_orderDetail';
 
 const tl_order = require("raw-loader!../../../templates/order/tl_order.html");
 
@@ -9,6 +10,10 @@ const OrderListView = Backbone.View.extend({
   initialize: function (params) {
     this.eventBus = params.eventBus;
     this.template = _.template(tl_order);
+
+    this.localEventBus = _.extend({}, Backbone.Events);
+    this.localEventBus.on('view:order:detail', this.detail.bind(this));
+
   },
 
   className: 'container',
@@ -16,11 +21,19 @@ const OrderListView = Backbone.View.extend({
   render: function () {
     this.$el.html(this.template({orders: this.collection}));
     const $orderList = this.$el.find('.list-group');
+    const eb = this.localEventBus;
     this.collection.each(function(order) {
-      $orderList.append(new OrderItemView({model: order}).render().el);
+      $orderList.append(new OrderItemView({model: order, eventBus: eb}).render().el);
     });
 
     return this;
+  },
+
+  detail: function(id) {
+    this.$el.find('.order-list').addClass('col-md-9').removeClass('col-md-12');
+    const $orderDetail = this.$el.find('.order-detail');
+    new OrderDetailView({el: $orderDetail, model: this.collection.get(id)}).render();
+    $orderDetail.show();
   }
 
 });
