1c7b347e1262851bc5c252a168971b9327280600
Ignacio Martín <natx.mc@gmail.com>
2019-03-05 13:51:01 +0100

    New order create view

---
+++ src/js/views/order/v_orderCreate.js
@@ -0,0 +1,43 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import Order from '~/models/m_order';
+
+const t_orderCreate = require("raw-loader!../../../templates/order/t_orderCreate.html");
+
+const OrderCreateView = Backbone.View.extend({
+
+    initialize: function (params) {
+        this.eventBus = params.eventBus;
+        this.template = _.template(t_orderCreate);
+    },
+
+    events: {
+        'click .remove-icon': 'hide',
+        'click #button-create' : 'createOrder'
+    },
+
+    render: function () {
+        this.$el.html(this.template({order: this.model}));
+        return this;
+    },
+
+    hide: function () {
+        this.eventBus.trigger('view:orderCreate:hide');
+    },
+
+    createOrder: function() {
+        const data = {
+            description: this.$el.find('[name="description"]').val()
+        };
+        const eventBus = this.eventBus;
+        const order = new Order();
+        order.save(data, {
+            success: function(order) {
+                eventBus.trigger('view:orderCreate:created', order);
+            }
+        });
+    }
+
+});
+
+export default OrderCreateView;
